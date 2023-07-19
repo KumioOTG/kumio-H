d268440e12b4b78ce0e71c547ee93e6f1ff2166d
Begum Moralioglu <begummoralioglu@gmail.com>
2023-07-19 04:31:51 -0700

    july23

---
+++ Assets/Editor/mUMeshEditor/mUMeshEditorMenu.cs
@@ -0,0 +1,19 @@
+﻿/*  Mesh Editor for Unity
+ *  Version 1.11
+ *  Created By Jihui Shentu
+ *  2014 All Rights Reserved
+ */
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using MU;
+
+public class mUMeshEditorMenu : MonoBehaviour {
+
+    [MenuItem("Window/mU Mesh Editor Panel", false, 0)]
+    public static void ShowWindow() {
+        EditorWindow window = EditorWindow.GetWindow(typeof(MeshEditor));
+        window.minSize = new Vector2(350, 450);
+    }
+}
